326730dc8213bb50fc87a6e974ef3af5a54137d8
Tadeáš Lauer <tadeas.lauer@gmail.com>
2026-05-24 19:46:36 +0200

    feat: add TheEntries shell with nested router-view

--- src/views/TheEntries.vue
+++ src/views/TheEntries.vue
@@ -1,1 +1,24 @@
-﻿<template><div /></template>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import EntryTabsNav from '@/components/EntryTabsNav.vue'
+</script>
+
+<template>
+  <section class="entries">
+    <EntryTabsNav />
+    <RouterView />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.entries {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 100px;
+  color: $color-text;
+
+  @media (max-width: $bp-md) {
+    padding: 0 1rem;
+  }
+}
+</style>
